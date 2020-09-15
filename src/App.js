05956f12df0277ed366b7fa91d54c98c912f2f7f
kellarelliott <kellarelliott@gmail.com>
2020-09-15 21:18:10 +0000

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const API_KEY = process.env.REACT_APP_EDAMAM_API_KEY;
-const API_ID = process.env.REACT_APP_EDAMAM_APP_ID;
+
 
 
 const App = () => {
+
+  const API_KEY = process.env.REACT_APP_EDAMAM_API_KEY;
+  const API_ID = process.env.REACT_APP_EDAMAM_APP_ID;
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  }, []);
+
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <form className='search-form'>
+        <input className='search-bar' type='text' />
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)} >{counter}</h1>
     </div>
-  )
-}
+  );
+};
 
 export default App;
