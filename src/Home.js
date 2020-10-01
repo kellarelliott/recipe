c0d0762edc7dec98e3a07e1958a17aa1269aef11
kellarelliott <kellarelliott@gmail.com>
2020-10-01 17:37:43 +0000

    Font Awsome

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import history from './history';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitterSquare, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
 
 
 const Home = () => {
@@ -22,14 +26,23 @@ const Home = () => {
 
   return (
     <div className='Home'>
-      <nav>Just the Recipe</nav>
+      <nav className='home-nav'>Just the Recipe</nav>
       <form onSubmit={getSearch} className='search-form' >
         <input className='search-bar' type='text' value={search} onChange={updateSearch} />
         <button className='search-button' type='submit'>
           Search
-          </button>
+        </button>
       </form>
-      <div className='foot'>Sup</div>
+      <div className='foot'>
+        <div style={{ padding: '2px' }}>
+          Developed by Kellar Elliott
+        </div>
+        <div>
+          <span>
+            <a href='https://github.com/kellarelliott'><FontAwesomeIcon icon={faGithubSquare} size='2x' className='icon' /></a>
+            <a href='https://www.linkedin.com/in/kellar-elliott/'><FontAwesomeIcon icon={faLinkedin} size='2x' className='icon' /></a></span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import './App.css'
 import Recipe from './Recipe';
-import history from './history';
 import Masonry from 'react-masonry-css';
+import { useHistory } from 'react-router-dom';
 
 const Search = ({ match }) => {
 
   const [recipes, setRecipes] = useState([])
   const [search, setSearch] = useState('')
   const [query, setQuery] = useState(match.params.thing)
+
+  const [locationKeys, setLocationKeys] = useState([])
+  const history = useHistory()
+
+  useEffect(() => {
+
+    return history.listen(location => {
+      if (history.action === 'PUSH') {
+        setLocationKeys([location.key]);
+        setQuery(location.pathname.slice(8));
+      }
+
+      if (history.action === 'POP') {
+        if (locationKeys[1] === location.key) {
+          setLocationKeys(([_, ...keys]) => keys);
+          setQuery(location.pathname.slice(8));
+        } else {
+          setLocationKeys((keys) => [location.key, ...keys]);
+          setQuery(location.pathname.slice(8));
+        }
+      }
+    })
+
+  }, [locationKeys,])
 
 
   const getRecipes = async () => {
@@ -29,7 +53,6 @@ const Search = ({ match }) => {
 
   const getSearch = e => {
     e.preventDefault();
-    console.log(query);
     setQuery(search);
     setSearch('');
     history.push(`/search/${search}`)
@@ -45,6 +68,15 @@ const Search = ({ match }) => {
 
   return (
     <div className='Search'>
+      <nav className='search-nav'>
+        <form onSubmit={getSearch} className='search-form' >
+          <input className='search-bar' type='text' value={search} onChange={updateSearch} />
+          <button className='search-button' type='submit'>
+            Search
+            </button>
+        </form>
+      </nav>
+
       <Masonry
         breakpointCols={breakpointColumnsObj}
         className="my-masonry-grid"
